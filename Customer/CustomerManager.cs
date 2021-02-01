c6f36d6927cc5ecc7579c9ec98921900c347c641
Şevval KALKAN <sevvalkalkan@eskisehir.edu.tr>
2021-02-02 00:21:39 +0300

    Add project files.

---
+++ Customer/CustomerManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Customer
+{
+    class CustomerManager
+    {
+        ArrayList customerList = new ArrayList(); 
+        public void Add (Customer customer)
+        {
+            customerList.Add(customer);
+            Console.WriteLine(customer.Name + " " +customer.Surname + "müşterisi listeye eklendi.");
+        }
+
+        public void List()
+        {
+            foreach (Customer customer in customerList)
+            {
+                Console.WriteLine(customer.Name + " " + customer.Surname);
+
+            }
+        }
+
+        public void Delete(Customer customer)
+        {
+            customerList.Remove(customer);
+            Console.WriteLine(customer.Name + " " + customer.Surname + "müşterisi listeden çıkarıldı.");
+        }
+    }
+}
---
+++ Customer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Customer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Monica";
+            customer1.Surname = "Geller";
+            customer1.Age = 27;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Chandler";
+            customer2.Surname = "Bing";
+            customer2.Age = 30;
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+            customerManager.List();
+            customerManager.Delete(customer1);
+            customerManager.List(); 
+        }
+    }
+}
